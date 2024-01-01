8a320cf7da7d048c911bc5a2dcd913b5d69c340c
Ranit Das <ranit@hexcms.hexfarm.rutgers.edu>
2024-01-01 07:01:44 -0500

    job array memory allocations fixed

--- job_array_training.sh
+++ job_array_training.sh
@@ -4,10 +4,9 @@
 #SBATCH --requeue
 #SBATCH --job-name=cross_validation
 #SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu=4G   # memory per cpu-core
 #SBATCH --time=3:00:00
 #SBATCH --gres=gpu:1
-#SBATCH --mem-per-gpu=1G
+#SBATCH --mem-per-gpu=5G
 
 
 source ~/.bashrc
@@ -22,3 +21,4 @@ cd /scratch/rd804/DisCo-FFS/
 
 
 python scripts/training_variance.py --split=${split} --iter=${iter} --exp_name=${exp_name} --${data}
+### #SBATCH --mem-per-cpu=4G   # memory per cpu-core
